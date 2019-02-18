4b97f1bc2cda6cbc05302d2d2a3fbfde41ac7381
HoverHell <hoverhell@gmail.com>
2019-02-18 21:33:37 +0300

    Extended example for comparison with http backend

--- example.py
+++ example.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # pylint: disable=invalid-name
 
+import os
 import sys
 
 DEBUG = True
@@ -15,7 +16,7 @@ def _dbg(msg):
 
 # Data needs to be downloaded and prepared. Example:
 # fln="central-fed-district-latest"
-# mode="foot"  # mode="driving"
+# mode="foot"  # mode="car"
 # mkdir osrm_data
 # cd osrm_data
 # wget "http://download.geofabrik.de/russia/${fln}.osm.pbf"
@@ -52,6 +53,7 @@ else:
 
 import numpy as np
 
+
 def test_route_matrix(num=10, mode='duration_seconds'):
     lon_step = (lon2 - lon1) / num
     lat_step = (lat2 - lat1) / num
@@ -59,6 +61,9 @@ def test_route_matrix(num=10, mode='duration_seconds'):
     from_lat_ar = np.arange(lat1, lat2, lat_step)
     to_lon_ar = np.full(num, lon2)  # constant
     to_lat_ar = np.arange(lat2, lat1, -lat_step)
+    return (
+        from_lon_ar, from_lat_ar,
+        to_lon_ar, to_lat_ar)
 
     mresult = worker.route_matrix(
         from_lon_ar=from_lon_ar,
@@ -69,20 +74,49 @@ def test_route_matrix(num=10, mode='duration_seconds'):
     )
     return mresult
 
-for mode in ('duration_seconds', 'distance_meters'):
-    mresult = test_route_matrix(mode=mode)
-    print(mode)
+test_routes = test_route_matrix()
+for ret_mode in ('duration_seconds', 'distance_meters'):
+    mresult = worker.route_matrix(*test_routes, mode=ret_mode)
+    print(ret_mode)
     print(mresult)
+    if os.environ.get('E_COMPARE_HTTP') and ret_mode == 'duration_seconds':
+        import itertools
+        import requests
+        import numpy as np
+        url_base = 'http://localhost:5000/table/v1/foot/'
+        url = url_base + ';'.join(
+            '{},{}'.format(lon, lat)
+            for lon, lat in itertools.chain(
+                    # from_lon, from_lat
+                    zip(test_routes[0], test_routes[1]),
+                    # to_lon, to_lat
+                    zip(test_routes[2], test_routes[3])))
+        params = dict(
+            sources=';'.join([str(val) for val in range(len(test_routes[0]))]),
+            destinations=';'.join([
+                str(val) for val in range(
+                    len(test_routes[0]),
+                    len(test_routes[0]) + len(test_routes[2]))]))
+        resp = requests.get(url, params=params)
+        resp.raise_for_status()
+        resp_data = resp.json()
+        assert resp_data['code'] == 'Ok'
+        resp_results = np.array(resp_data['durations'])
+        assert np.array_equal(mresult, resp_results)
 
-print("perf-test...")
-import time
-perf_num = 100
-t1 = time.time()
-mresult = test_route_matrix(num=100)
-t2 = time.time()
-print("Time for {size}x{size} matrix: {timing:.3f}".format(
-    size=perf_num,
-    timing=t2 - t1))
+
+if not os.environ.get('E_SKIP_PERFTEST'):
+    print("perf-test...")
+    import time
+    perf_num = 100
+    perf_data = test_route_matrix(num=perf_num)
+    t1 = time.time()
+    mresult = worker.route_matrix(*perf_data, mode='duration_seconds')
+    t2 = time.time()
+    print("Time for {size}x{size} matrix: {timing:.3f}".format(
+        size=perf_num,
+        timing=t2 - t1))
+
 _dbg('cleanup')
 del worker
 print("...")
